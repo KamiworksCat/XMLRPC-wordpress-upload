70df4cdcfd621b5eb26dad28233a44d7d59d5f93
catcoder <waikeat@kamiworks.com>
2016-04-26 15:33:57 +0800

    [fabfile.py]Updating fabfile with settings configuration

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,6 @@ from fabric.api import *
 
 def build():
     with lcd('src/Bolt_wordpresspublish'):
-        local('python ./manage.py migrate --noinput')
+        local('python ./manage.py migrate --noinput --settings=conf.settings.local')
         with lcd('static/'):
             local('bower install')
